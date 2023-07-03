fc687c035ea4fc9fdf05e0a938d2c571ce5cd7d8
luiz Felipe <luizmachadoathayde@gmail.com>
2023-07-03 17:50:25 -0300

    login 3.0

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -23,3 +23,49 @@ class LoginForms(forms.Form):
             }
         )
     )
+
+class cadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome de Cadastro",
+        required=True,
+        max_length=100,
+        widget= forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "bota teu nome ai parça"
+            }
+        )
+    )
+    email= forms.EmailField(
+        label="bota seu Gmail ai",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+                attrs={
+                "class": "form-control",
+                "placeholder": "bota teu gmail ai parça"
+            }
+        )
+    )
+    senha_1= forms.CharField(
+        label="Senha",
+        required = True,
+        max_length=70,
+        widget= forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "sua senha ai"
+            }
+        )
+    )
+    senha_2= forms.CharField(
+        label="Confirme sua senha",
+        required = True,
+        max_length=70,
+        widget= forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "mais uma vez mestre"
+            }
+        )
+    )
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from usuarios.forms import LoginForms
+from usuarios.forms import LoginForms,cadastroForms
 
 def login ( request):
     form = LoginForms ()
@@ -8,3 +8,18 @@ def login ( request):
 
 def cadastro(request):
     return render(request, "usuarios/cadastro.html")
+
+def cadastro(request):
+    form = cadastroForms()
+    if request.method == 'POST':
+        form= cadastroForms(request.POST)
+
+        if form["senha_1"].value() != form["senha_2"].value():
+            return redirect('cadastro')
+        
+    
+    
+    
+    
+    else:
+        return render (request, "usuarios/cadastro.html", {"form":form})
