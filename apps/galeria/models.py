26f1831f53294d4f39e4c6b7a212317c7e5fd546
luiz Felipe <luizmachadoathayde@gmail.com>
2023-07-25 17:56:21 -0300

    editar imagem

--- apps/galeria/models.py
+++ apps/galeria/models.py
@@ -7,10 +7,10 @@ from django.contrib.auth.models import User
 class Fotografia (models.Model):
     
     OPCOES_CATEGORIA=[
-        ("NEBULOSA","Nebulosa"),
-        ("ESTRELA","Estrela"),
-        ("GALAXIA","Galaxia"),
-        ("PLANETA","Planeta"),  
+        ("ENGRAÇADA","Engraçada"),
+        ("CIENTIFICA","Cientifica"),
+        ("ASSUSTADORA","Assustadora"),
+        ("BADASS","Badass"),  
     ]   
     
     nome = models.CharField(max_length=100, null=False, blank=False)
@@ -18,7 +18,7 @@ class Fotografia (models.Model):
     categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
     descricao = models.TextField(null=False, blank=False)
     foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
-    publicada = models.BooleanField(default=False)
+    publicada = models.BooleanField(default=True)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
     usuario = models.ForeignKey(
         to = User,
